f1c89cc44a40e8bb7fdf574122e07131fca74645
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-08-20 16:30:19 +0200

    Revise benchmarks
    When running benchmarks, the main input metric that determines
    performance should be input string length.

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -10,65 +10,28 @@
 #include <cstring>
 
 template<typename T>
-constexpr const char *input();
-
-template<>
-constexpr const char *input<std::int32_t>()
-{
-  return "-12345";
-}
-
-template<>
-constexpr const char *input<std::int64_t>()
-{
-  return "-223372036854775808";
-}
-
-template<>
-constexpr const char *input<std::uint64_t>()
-{
-  return "9223372036854775808";
-}
-
-template<typename T>
 static void BM_fast_int(benchmark::State &state)
 {
-  const char *number{input<T>()};
-  const char *const last{number + std::strlen(number)};
-  T sum{};
+  const std::string s{std::to_string(state.range(0))};
 
   for (auto _ : state)
   {
     T i{};
-    fast_int::from_chars(number, last, i);
-    benchmark::DoNotOptimize(sum += i);
+    fast_int::from_chars(s.data(), s.data() + s.size(), i);
+    benchmark::DoNotOptimize(i);
   }
 }
 
 template<typename T>
 static void BM_strtol(benchmark::State &state)
 {
-  const char *number{input<T>()};
-  T sum{};
+  const std::string s{std::to_string(state.range(0))};
 
   for (auto _ : state)
   {
     T i{};
-    std::strtol(number, nullptr, 10);
-    benchmark::DoNotOptimize(sum += i);
-  }
-}
-
-template<typename T>
-static void BM_atol(benchmark::State &state)
-{
-  const char *number{input<T>()};
-  T sum{};
-
-  for (auto _ : state)
-  {
-    T i = std::atol(number);
-    benchmark::DoNotOptimize(sum += i);
+    std::strtol(s.data(), nullptr, 10);
+    benchmark::DoNotOptimize(i);
   }
 }
 
@@ -76,15 +39,13 @@ static void BM_atol(benchmark::State &state)
 template<typename T>
 static void BM_fast_float(benchmark::State &state)
 {
-  const char *number{input<T>()};
-  const char *last{number + strlen(number)};
-  double sum{};
+  const std::string s{std::to_string(state.range(0))};
 
   for (auto _ : state)
   {
     double i{};
-    fast_float::from_chars(number, last, i);
-    benchmark::DoNotOptimize(sum += i);
+    fast_float::from_chars(s.data(), s.data() + s.size(), i);
+    benchmark::DoNotOptimize(i);
   }
 }
 #endif
@@ -92,40 +53,30 @@ static void BM_fast_float(benchmark::State &state)
 template<typename T>
 static void BM_from_chars(benchmark::State &state)
 {
-  const char *number{input<T>()};
-  const char *const last{number + std::strlen(number)};
-  T sum{};
+  const std::string s{std::to_string(state.range(0))};
 
   for (auto _ : state)
   {
     T i{};
-    std::from_chars(number, last, i);
-    benchmark::DoNotOptimize(sum += i);
+    std::from_chars(s.data(), s.data() + s.size(), i);
+    benchmark::DoNotOptimize(i);
   }
 }
 
-BENCHMARK(BM_fast_int<std::int32_t>);
-BENCHMARK(BM_from_chars<std::int32_t>);
-BENCHMARK(BM_strtol<std::int32_t>);
-BENCHMARK(BM_atol<std::int32_t>);
+BENCHMARK(BM_fast_int<std::int64_t>)->RangeMultiplier(10)->Range(std::int64_t{1}, 1000000);
+BENCHMARK(BM_strtol<std::int64_t>)->RangeMultiplier(10)->Range(std::int64_t{1}, 1000000);
+BENCHMARK(BM_from_chars<std::int64_t>)->RangeMultiplier(10)->Range(std::int64_t{1}, 1000000);
+
 #ifdef WITH_FAST_FLOAT
-BENCHMARK(BM_fast_float<std::int32_t>);
+BENCHMARK(BM_fast_float<std::int64_t>)->RangeMultiplier(10)->Range(std::int64_t{1}, 1000000);
 #endif
 
-BENCHMARK(BM_fast_int<std::int64_t>);
-BENCHMARK(BM_from_chars<std::int64_t>);
-BENCHMARK(BM_strtol<std::int64_t>);
-BENCHMARK(BM_atol<std::int64_t>);
-#ifdef WITH_FAST_FLOAT
-BENCHMARK(BM_fast_float<std::int64_t>);
-#endif
+BENCHMARK(BM_fast_int<std::uint64_t>)->RangeMultiplier(10)->Range(std::uint64_t{1}, 1000000);
+BENCHMARK(BM_strtol<std::uint64_t>)->RangeMultiplier(10)->Range(std::uint64_t{1}, 1000000);
+BENCHMARK(BM_from_chars<std::uint64_t>)->RangeMultiplier(10)->Range(std::uint64_t{1}, 1000000);
 
-BENCHMARK(BM_fast_int<std::uint64_t>);
-BENCHMARK(BM_from_chars<std::uint64_t>);
-BENCHMARK(BM_strtol<std::uint64_t>);
-BENCHMARK(BM_atol<std::uint64_t>);
 #ifdef WITH_FAST_FLOAT
-BENCHMARK(BM_fast_float<std::uint64_t>);
+BENCHMARK(BM_fast_float<std::uint64_t>)->RangeMultiplier(10)->Range(std::uint64_t{1}, 1000000);
 #endif
 
 BENCHMARK_MAIN();
